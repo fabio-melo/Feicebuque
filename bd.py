3d336338d33be9255b98593c2d246a33bc5c8552
Fábio Melo <fabio-melo@users.noreply.github.com>
2018-06-02 11:16:01 -0300

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,73 +1,157 @@
-import json,datetime,time,csv,sys,random
+import json,datetime,time,csv,random
+from sys import argv
 import MySQLdb as mariadb
+import config
+from mussum import mussunificador
+from query import *
 
 
-def init_db():
-    cursor.execute(open('schema.sql', mode='r',encoding="utf8").read()); print("IT WORKS")
+
+
+def init_db(): 
+    cursor.execute(open('schema.sql', mode='r',encoding="utf8").read())
+    print("Banco de Dados Inicializado")
+
+def restartconn():
+    if 'db' in globals():
+        globals()['db'].close()
+
+    db = mariadb.connect(**config.DATABASE_CONFIG)
+    cursor = db.cursor()
+    return db, cursor
 
 def import_test_data():
     cursor.execute("USE social;")
     with open('MOCK_DATA.csv', 'r') as file:
         reader = csv.DictReader(file)
         for x in reader:
-            cursor.execute("INSERT INTO pessoas(nome, sobrenome,email,senha,bio) VALUES (%s,%s,%s,%s,%s);", (x['first_name'],x['last_name'],x['email'],x['password'],x['bio']) )
+            cursor.execute(DB_CRIAR_USUARIO, (x['first_name'],x['last_name'],x['email'],x['gender'],x['password'],x['bio']) )
         #for x in range(50):
-        #    cursor.execute("INSERT INTO p_amigos(id_amigo1, id_amigo2) VALUES (%s,%s);", (random.randint(1,1000),random.randint(1,1000)))
+        #    for y in range(random.randint(1,10)): 
+        #        cursor.execute(DB_ADICIONAR_AMIGO, (int(x), int(y+1)))
     db.commit()
-    print("IMPORTED")
+    import_test_publications(qtdpub=200,qtdpessoa=100)
+    print("Dados de Teste Importados")
+
+
+def import_test_publications(qtdpub=1, qtdpessoa=5):
+    for _ in range(qtdpub):
+        escrever_publicacao(random.randint(1,qtdpessoa), mussunificador())
 
 #-----------------------------------------#
-# Operações de Amizade (p_amigos)         #
+# (pessoas)                               #
 #-----------------------------------------#
-def adicionar_amigo(id_amigo1,id_amigo2):
+def criar_usuario(nome, sobrenome='',email='', genero=None, senha='', bio=''):
     try:
-        par_amigos = sorted([int(id_amigo1), int(id_amigo2)])
-        cursor.execute("INSERT INTO p_amigos(id_amigo1,id_amigo2) VALUES (%s,%s);", (par_amigos[0],par_amigos[1]))
+        cursor.execute(DB_CRIAR_USUARIO, (nome, sobrenome, email, genero, senha, bio))
         db.commit()
-    except (mariadb.Error, mariadb.Warning) as e:
-        print(e)
+    except:
+        print("ERRO CRIANDO USUÁRIO")
         return None
 
-def remover_amigo(id_amigo1,id_amigo2):
+def excluir_usuario(id_pessoa):
     try:
-        par_amigos = sorted([int(id_amigo1), int(id_amigo2)])
-        cursor.execute("DELETE FROM p_amigos WHERE id_amigo1 = %s AND id_amigo2 = %s;", (par_amigos[0],par_amigos[1]))
+        cursor.execute(DB_EXCLUIR_USUARIO, (id_pessoa))
         db.commit()
-    except (mariadb.Error, mariadb.Warning) as e:
-        print(e)
+    except:
+        print("ERRO EXCLUINDO USUÁRIO")
+
+#-----------------------------------------#
+# (p-amigos)                              #
+#-----------------------------------------#
+
+def adicionar_amigo(id_pessoa1,id_pessoa2):
+    try:
+        par_amigos = sorted([int(id_pessoa1), int(id_pessoa2)])
+        cursor.execute(DB_ADICIONAR_AMIGO, (par_amigos[0],par_amigos[1]))
+        db.commit()
+    except:
+        return None
+
+def remover_amigo(id_pessoa1,id_pessoa2):
+    try:
+        par_amigos = sorted([int(id_pessoa1), int(id_pessoa2)])
+        cursor.execute(DB_REMOVER_AMIGO, (par_amigos[0],par_amigos[1]))
+        db.commit()
+    except:
         return None
 
 def listar_amigos(id_pessoa):
     try:
-        cursor.execute("SELECT * FROM p_amigos WHERE (id_amigo1 LIKE %s OR id_amigo2 LIKE %s);", (int(id_pessoa),int(id_pessoa)))
+        cursor.execute(DB_LISTAR_AMIGOS, (int(id_pessoa),int(id_pessoa)))
         lista = cursor.fetchall()
-        print(lista)
         db.commit()
-    except (mariadb.Error, mariadb.Warning) as e:
-        print(e)
+        return lista
+    except:
+        print("ERRO Listando Amigos")
         return None    
 
 #-----------------------------------------#
-# Operações de Bloqueio (p_bloqueado)     #
+# (p-amigos)                              #
 #-----------------------------------------#
+
+def bloquear_pessoa(id_pessoa1,id_pessoa2):
+    try:
+        par_pessoas = sorted([int(id_pessoa1), int(id_pessoa2)])
+        cursor.execute(DB_BLOQUEAR_PESSOA, (par_pessoas[0],par_pessoas[1]))
+        cursor.execute(DB_REMOVER_AMIGO, (par_pessoas[0],par_pessoas[1]))
+        db.commit()
+    except:
+        return None
+
+def desbloquear_pessoa(id_pessoa1,id_pessoa2):
+    try:
+        par_pessoas = sorted([int(id_pessoa1), int(id_pessoa2)])
+        cursor.execute(DB_DESBLOQUEAR_PESSOA, (par_pessoas[0],par_pessoas[1]))
+        db.commit()
+    except:
+        return None
+
+def listar_bloqueios(id_pessoa):
+    try:
+        cursor.execute(DB_LISTAR_BLOQUEIOS, (int(id_pessoa),int(id_pessoa)))
+        lista = cursor.fetchall()
+        db.commit()
+        return lista
+    except:
+        print("ERRO Listando Bloqueios")
+        return None    
+
+#-----------------------------------------#
+# (p_publicacoes)                         #
+#-----------------------------------------#
+
+
+def escrever_publicacao(id_pessoa, texto):
+    try:
+        cursor.execute(DB_ESCREVER_PUBLICACAO, (int(id_pessoa), texto))
+        db.commit()
+    except:
+        print("ERRO Escrevendo Publicacao")
+        return None
+
+def excluir_publicacao(id_publicacao):
+    try:
+        cursor.execute(DB_REMOVER_PUBLICACAO, id_publicacao)
+        db.commit()
+    except:
+        print("erro excluindo publicacao")
+
 
 
 
 
 
 if __name__ == "__main__":
-    db = mariadb.connect(user='root', password='@idk')
-    #db.autocommit = True
-    cursor = db.cursor()
-    
-    op = sys.argv[1] if len(sys.argv) >= 2 else 'noop'
-    
-    if op == 'initdb': init_db(); exit()
-    if op == 'import': import_test_data()
+    db, cursor = restartconn()
 
-    cursor.execute("USE social;")  
+    if 'initdb' in argv: init_db(); db, cursor = restartconn()
+    if 'import' in argv: import_test_data(); db, cursor = restartconn()
 
-    if op == 'test':    
+    # 
+
+    if 'test1' in argv:
+        cursor.execute("USE social;") 
         adicionar_amigo(1,4)
         adicionar_amigo(1,5)
         adicionar_amigo(1,6)
@@ -81,4 +165,14 @@ if __name__ == "__main__":
         adicionar_amigo(9,3)
         adicionar_amigo(8,5)
         remover_amigo(8,5)
-    if op == 'lis': listar_amigos(11)
+        bloquear_pessoa(9,5)
+    if 'tes' in argv: 
+        cursor.execute("USE social;") 
+        #listar_amigos(1)
+        #escrever_publicacao(1, mussunificador())
+        #escrever_publicacao(2, mussunificador())
+        #criar_usuario(nome="John", genero='Masculino')
+
+    if 'bla' in argv:
+        cursor.execute("USE social;") 
+        excluir_usuario('1')
