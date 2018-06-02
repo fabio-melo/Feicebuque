0159a7b04d5dad7a419c1867930f8710337d5930
Fábio Melo <fabio-melo@users.noreply.github.com>
2018-06-02 11:19:40 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,49 +1,12 @@
-#Fonte: http://adamlamers.com/post/GRBJUKCDMPOA
+from random import shuffle
 
-def parse_sql(filename):
-    data = open(filename, 'r',encoding='utf8').readlines()
-    stmts = []
-    DELIMITER = ';'
-    stmt = ''
+def mussum(tamanhotexto=5):
+    mussumfala = ["Pra lá , depois divoltis porris, paradis. ", "Paisis, filhis, espiritis santis. ", "Mé faiz elementum girarzis, nisi eros vermeio. ", "Manduma pindureta quium dia nois paga. ", "Sapien in monti palavris qui num significa nadis i pareci latim. ", "Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.", "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. ", "Interagi no mé, cursus quis, vehicula ac nisi. ", "Casamentiss faiz malandris se pirulitá.", "Cevadis im ampola pa arma uma pindureta. ", "Atirei o pau no gatis, per gatis num morreus. ", "Viva Forevis aptent taciti sociosqu ad litora torquent. ", "Copo furadis é disculpa de bebadis, arcu quam euismod magna. ", "Delegadis gente finis, bibendum egestas augue arcu ut est. ", "In elementis mé pra quem é amistosis quis leo. ", "Não sou faixa preta cumpadi, sou preto inteiris, inteiris. ", "Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. ", "Suco de cevadiss deixa as pessoas mais interessantis. ", "Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. ", "Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! ", "Quem manda na minha terra sou euzis! ", "Si num tem leite então bota uma pinga aí cumpadi! ", "Diuretics paradis num copo é motivis de denguis. ", "Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. ", "A ordem dos tratores não altera o pão duris. ", "Quem num gosta di mim que vai caçá sua turmis! ", "Quem num gosta di mé, boa gentis num é. ", "Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! ", "Per aumento de cachacis, eu reclamis. ", "Detraxit consequat et quo num tendi nada. ", "Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. ", "Leite de capivaris, leite de mula manquis sem cabeça. ", "Aenean aliquam molestie leo, vitae iaculis nisl. ", "Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. ", "Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. ", "Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. ", "Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. ", "Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. ", "Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. ", "Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. "]
 
-    for lineno, line in enumerate(data):
-        if not line.strip():
-            continue
+    shuffle(mussumfala)
+    mussumstring = []
+    if tamanhotexto > 40: print("Deu errosis"); exit()
+    for _ in range(tamanhotexto):
+        mussumstring.append(mussumfala.pop())
+    return ''.join(mussumstring)
 
-        if line.startswith('--'):
-            continue
-
-        if 'DELIMITER' in line:
-            DELIMITER = line.split()[1]
-            continue
-
-        if (DELIMITER not in line):
-            stmt += line.replace(DELIMITER, ';')
-            continue
-
-        if stmt:
-            stmt += line
-            stmts.append(stmt.strip())
-            stmt = ''
-        else:
-            stmts.append(line.strip())
-    return stmts
-
-
-def run_sql_file(filename, connection):
-    '''
-    The function takes a filename and a connection as input
-    and will run the SQL query on the given connection  
-    '''
-    #start = time.time()
-    
-    file = open(filename, 'r',encoding='utf8')
-    sql = s = " ".join(file.readlines()).rstrip()
-    #print "Start executing: " + filename + " at " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")) + "\n" + sql 
-    cursor = connection.cursor()
-    cursor.execute(sql)    
-    connection.commit()
-    
-   # end = time.time()
-   # print "Time elapsed to run the query:"
-    #print str((end - start)*1000) + ' ms'
